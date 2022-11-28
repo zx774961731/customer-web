4fe40d52c797cd5e4dad08be712e0f31ef63cc3c
zx <774961731@qq.com>
2022-11-28 15:40:28 +0800

    初始化

---
+++ src/views/demo/route.js
@@ -0,0 +1,49 @@
+const Layout = () => import('@/layout/index.vue')
+
+export default {
+  name: 'Demo',
+  path: '/demo',
+  component: Layout,
+  redirect: '/demo/createOrder',
+  isHidden: true,
+  meta: {
+    title: '用户',
+    customIcon: 'logo',
+    role: ['admin'],
+    requireAuth: true,
+    order: 3,
+  },
+  children: [
+    {
+      name: 'createOrder',
+      path: 'createOrder',
+      component: () => import('./CreateOrder.vue'),
+      meta: {
+        title: '创建工单',
+        icon: 'ic:baseline-table-view',
+        role: ['admin'],
+        requireAuth: true,
+        keepAlive: true,
+      },
+    },
+    {
+      name: 'orderDetail',
+      path: '/orderDetail',
+      component: () => import('./OrderDetail.vue'),
+      isHidden: true,
+      content: '工单详情',
+    },
+    {
+      name: 'recordTable',
+      path: 'record-table',
+      component: () => import('./table/record-list.vue'),
+      meta: {
+        title: '我的服务记录',
+        icon: 'ic:baseline-table-view',
+        role: ['admin'],
+        requireAuth: true,
+        keepAlive: true,
+      },
+    },
+  ],
+}
